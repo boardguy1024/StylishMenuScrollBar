285a98e58db730ddefee945ce7047f137424e7e7
park kyung suk <boardguy1024@gmail.com>
2018-10-25 15:01:19 +0900

    リソース追加

--- StylishMenuScrollBar/ViewController.swift
+++ StylishMenuScrollBar/ViewController.swift
@@ -10,9 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menyScrollView: UIScrollView!
+    
+    let cardWidth: CGFloat  = 300
+    let cardHeight: CGFloat = 300
+    var cardViews: [UIView] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        initScrollView()
+    }
+    
+    private func initScrollView() {
+        cardViews.append(createCardViewFromImageName("image1", index: 0))
+        cardViews.append(createCardViewFromImageName("image2", index: 1))
+        cardViews.append(createCardViewFromImageName("image3", index: 2))
+        cardViews.forEach {
+            scrollView.addSubview($0)
+        }
+        scrollView.contentSize = CGSizeMake(view.frame.width * CGFloat(cardViews.count), cardHeight)
+    }
+    
+    private func createCardViewFromImageName(imageName: String, index: Int) -> UIView {
+        let imageView = UIImageView(frame: CGRectMake(0, 0, cardWidth, cardHeight))
+        imageView.image = UIImage(named: imageName)?.roundedImage(5)
+        imageView.contentMode = UIViewContentMode.ScaleAspectFit
+        imageView.center.x = view.frame.width / 2
+        let cardX: CGFloat = CGFloat(index) * view.frame.width
+        let cardView = UIView(frame: CGRectMake(cardX, 0, view.frame.width, cardHeight))
+        cardView.addSubview(imageView)
+        return cardView
     }
 
 
